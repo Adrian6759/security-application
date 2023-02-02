f82b6435ec48eaebdc873f464071a321d713a2db
adrian.hernandez1993@yahoo.com <adrianhernandez@Adrians-MacBook-Pro-2.local>
2023-02-01 23:26:08 -0800

    Added some functionality, still needs work

--- src/main/java/com/codefellowship/d16Security/models/SiteUser.java
+++ src/main/java/com/codefellowship/d16Security/models/SiteUser.java
@@ -3,12 +3,11 @@ package com.codefellowship.d16Security.models;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 //Step 1b: Make user model (DO NOT CALL IT USER)
 @Entity
@@ -19,17 +18,50 @@ public class SiteUser implements UserDetails {
     private String username;
     private String password;
     private String firstName;
+    private String lastName;
     private Date dateOfBirth;
+    private String bio;
 
     //Create Default Constructor
 
     protected SiteUser() {}
 
-    public SiteUser(String username, String password, String firstName, Date dateOfBirth) {
+    @ManyToMany
+    @JoinTable (
+            name = "followers_to_followees",
+            joinColumns =  {@JoinColumn(name = "userWhoIsFollowing")},
+            inverseJoinColumns = {@JoinColumn(name = "FollowedUser")}
+    )
+    Set<SiteUser> usersIFollow = new HashSet<>();
+
+    @ManyToMany(mappedBy = "usersIFollow")
+    Set<SiteUser> usersWhoFollowMe = new HashSet<>();
+
+    public Set<SiteUser> getUsersIFollow() {
+        return usersIFollow;
+    }
+
+    public Set<SiteUser> getUsersWhoFollowMe() {
+        return usersWhoFollowMe;
+    }
+    @OneToMany(mappedBy = "postsIveMade")
+    Set<Posts> listOfPosts = new HashSet<>();
+
+    public Set<Posts> getListOfPosts() {
+        return listOfPosts;
+    }
+
+    public void setListOfPosts(Set<Posts> listOfPosts) {
+        this.listOfPosts = listOfPosts;
+    }
+
+    public SiteUser(String username, String password, String firstName, String lastName, Date dateOfBirth, String bio) {
         this.username = username;
         this.password = password;
         this.firstName = firstName;
+        this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
+        this.bio = bio;
     }
 
     public Long getId() {
@@ -87,6 +119,22 @@ public class SiteUser implements UserDetails {
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
     }
 
     public Date getDateOfBirth() {
